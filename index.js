508a8fdfecc44f6c04e1e279a1a8b511afe14561
Massad Ilyas <+>
2023-02-22 09:21:38 -0500

    updated Lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Write your code in this file!
+const currentUser='Grace Hopper';
+const welcomeMessage= 'Welcome to Flatbook, ${currentUser}!';
+const excitedWelcomeMessage= welcomeMessage.toUpperCase();
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
+
+
+'Edsger Dijkstra'.slice();
+//=> Edsger Dijkstra
+
+'Edsger Dijkstra'.slice(7);
+//=> Dijkstra
+
+'Edsger Dijkstra'.slice(0,3);
+//=> Eds
+currentUser.slice(0,1);

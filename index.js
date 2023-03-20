f9988f44ed5d4ff0c134eb218a5bb936ead0d5f5
Massad Ilyas <+>
2023-03-20 16:06:24 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Write your code in this file!
 const currentUser='Grace Hopper';
-const welcomeMessage= 'Welcome to Flatbook, ${currentUser}!';
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 const excitedWelcomeMessage= welcomeMessage.toUpperCase();
 const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
 
@@ -14,3 +15,4 @@ const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
 'Edsger Dijkstra'.slice(0,3);
 //=> Eds
 currentUser.slice(0,1);
+
